d44c169039c14d9a77c3713e6bb5216e401b54b4
Ikuo Degawa <degawa@ikuwow.com>
2015-03-24 23:26:30 +0900

    Create first index page

--- config/routes.php
+++ config/routes.php
@@ -52,7 +52,8 @@ Router::scope('/', function ($routes) {
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
      */
-    $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
+    // $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
+    $routes->connect('/pages/*', ['controller' => 'Pages']);
 
     /**
      * Connect catchall routes for all controllers.
--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -62,4 +62,7 @@ class PagesController extends AppController
             throw new NotFoundException();
         }
     }
+
+    public function index() {
+    }
 }
